6f18ce4e220cc0bcf683e95e5d04a9fd8ab54de5
bbldn05 <bobol.boris@gmail.com>
2021-09-01 06:34:35 +0300

    01.09.2021 06:34

---
+++ src/components/Main.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="main">
+    <a-layout>
+      <a-layout-content>Content</a-layout-content>
+    </a-layout>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Main',
+}
+</script>
