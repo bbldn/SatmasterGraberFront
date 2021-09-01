5a43b0265e31d2d89f6f48021ea17f497020df05
bbldn05 <bobol.boris@gmail.com>
2021-09-01 07:26:17 +0300

    01.09.2021 07:26

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,13 +1,30 @@
 <template>
-  <div id="main">
-    <a-layout>
-      <a-layout-content>Content</a-layout-content>
-    </a-layout>
-  </div>
+    <div id="main">
+        <a-tabs default-active-key="1">
+            <a-tab-pane key="1" tab="Настройка">
+                <Form/>
+            </a-tab-pane>
+            <a-tab-pane key="2" tab="Прогресс" disabled>
+                <Form/>
+            </a-tab-pane>
+        </a-tabs>
+    </div>
 </template>
 
 <script>
+import Form from "./form/Form";
+
 export default {
-  name: 'Main',
+    name: 'Main',
+    components: {Form},
 }
 </script>
+
+<style>
+#main {
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+</style>
---
+++ src/components/form/Form.vue
@@ -0,0 +1,52 @@
+<template>
+    <a-form-model :model="form">
+        <a-form-item label="Ссылка на категорию satmaster.kiev.ua">
+            <a-input
+                v-model="form.url"
+                placeholder="Пожайлуста, введите ссылку на категорию в satmaster.kiev.ua"
+                v-decorator="['note', { rules: [{ required: true, message: 'Пожайлуста, введите ссылку на категорию в satmaster.kiev.ua' }] }]"
+            />
+        </a-form-item>
+        <a-form-item label="Идентификатор категории в t2.kiev.ua">
+            <a-input-number
+                :min="1"
+                v-model="form.categoryId"
+                placeholder="Пожайлуста, введите идентификатор категории в t2.kiev.ua"
+                v-decorator="['note', { rules: [{ required: true, message: 'Пожайлуста, введите идентификатор категории в t2.kiev.ua' }] }]"
+            />
+        </a-form-item>
+        <a-form-item label="Путь для картинок">
+            <a-input
+                v-model="form.imagePath"
+                placeholder="Пожайлуста, путь для картинок"
+                v-decorator="['note', { rules: [{ required: true, message: 'Пожайлуста, путь для картинок' }] }]"
+            />
+        </a-form-item>
+        <a-form-item>
+            <a-button type="primary" html-type="submit">
+                Начать
+            </a-button>
+        </a-form-item>
+    </a-form-model>
+</template>
+
+<script>
+export default {
+    name: "Form",
+    data: function () {
+        return {
+            form: {
+                url: '',
+                categoryId: 1,
+                imagePath: 'catalog/prod/graber/',
+            },
+        };
+    },
+}
+</script>
+
+<style scoped>
+a-form {
+    max-width: 800px;
+}
+</style>
